b8cd6cc1264e8a67489716626f54bf43be86b269
codeSTACKr <codestackr@gmail.com>
2021-10-20 11:53:35 -0500

    add dashboard link to user menu

--- components/dashboard/header/UserMenu.js
+++ components/dashboard/header/UserMenu.js
@@ -1,6 +1,7 @@
-import { useState, useRef, useEffect } from 'react';
-import { useUser } from '@auth0/nextjs-auth0';
-import Transition from '../../../utils/Transition';
+import { useState, useRef, useEffect } from "react";
+import { useUser } from "@auth0/nextjs-auth0";
+import Link from "next/link";
+import Transition from "../../../utils/Transition";
 
 // import UserAvatar from '../../images/user-avatar-32.png';
 
@@ -14,11 +15,16 @@ function UserMenu() {
   // close on click outside
   useEffect(() => {
     const clickHandler = ({ target }) => {
-      if (!dropdownOpen || dropdown.current.contains(target) || trigger.current.contains(target)) return;
+      if (
+        !dropdownOpen ||
+        dropdown.current.contains(target) ||
+        trigger.current.contains(target)
+      )
+        return;
       setDropdownOpen(false);
     };
-    document.addEventListener('click', clickHandler);
-    return () => document.removeEventListener('click', clickHandler);
+    document.addEventListener("click", clickHandler);
+    return () => document.removeEventListener("click", clickHandler);
   });
 
   // close if the esc key is pressed
@@ -27,11 +33,11 @@ function UserMenu() {
       if (!dropdownOpen || keyCode !== 27) return;
       setDropdownOpen(false);
     };
-    document.addEventListener('keydown', keyHandler);
-    return () => document.removeEventListener('keydown', keyHandler);
+    document.addEventListener("keydown", keyHandler);
+    return () => document.removeEventListener("keydown", keyHandler);
   });
 
-  if(!user) {
+  if (!user) {
     return (
       <a
         className="mt-3 text-gray-600 hover:underline sm:mx-3 sm:mt-0"
@@ -39,7 +45,7 @@ function UserMenu() {
       >
         Login
       </a>
-    )
+    );
   }
 
   return (
@@ -51,17 +57,28 @@ function UserMenu() {
         onClick={() => setDropdownOpen(!dropdownOpen)}
         aria-expanded={dropdownOpen}
       >
-        <img className="w-8 h-8 rounded-full" src={user.picture} width="32" height="32" alt="User" />
+        <img
+          className="w-8 h-8 rounded-full"
+          src={user.picture}
+          width="32"
+          height="32"
+          alt="User"
+        />
         <div className="flex items-center truncate">
-          <span className="truncate ml-2 text-sm font-medium group-hover:text-gray-800">{user.nickname}</span>
-          <svg className="w-3 h-3 flex-shrink-0 ml-1 fill-current text-gray-400" viewBox="0 0 12 12">
+          <span className="truncate ml-2 text-sm font-medium group-hover:text-gray-800">
+            {user.nickname}
+          </span>
+          <svg
+            className="w-3 h-3 flex-shrink-0 ml-1 fill-current text-gray-400"
+            viewBox="0 0 12 12"
+          >
             <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
           </svg>
         </div>
       </button>
 
       <Transition
-        className="origin-top-right z-10 absolute top-full right-0 min-w-44 bg-white border border-gray-200 py-1.5 rounded shadow-lg overflow-hidden mt-1"
+        className="origin-top-right z-10 absolute top-full right-0 min-w-44 bg-white border border-gray-200 py-1.5 rounded shadow-lg overflow-hidden mt-1 z-50"
         show={dropdownOpen}
         enter="transition ease-out duration-200 transform"
         enterStart="opacity-0 -translate-y-2"
@@ -80,6 +97,16 @@ function UserMenu() {
             {/* <div className="text-xs text-gray-500 italic">Administrator</div> */}
           </div>
           <ul>
+            <li>
+              <Link
+                href="/dashboard"
+                onClick={() => setDropdownOpen(!dropdownOpen)}
+              >
+                <a className="font-medium text-sm text-green-500 hover:text-green-600 flex items-center py-1 px-3">
+                  Dashboard
+                </a>
+              </Link>
+            </li>
             <li>
               <a
                 className="font-medium text-sm text-green-500 hover:text-green-600 flex items-center py-1 px-3"
@@ -102,7 +129,7 @@ function UserMenu() {
         </div>
       </Transition>
     </div>
-  )
+  );
 }
 
 export default UserMenu;
